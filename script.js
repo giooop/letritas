613c5234eb98c03a272242805de0fb1652b319bb
gioo <giovamassardi@gmail.com>
2024-04-22 01:03:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+$(document).ready(function () {
+    /*
+     * Main variables
+     */
+    var content = [{
+        title: "¡Te amo mucho!",
+        desc: ""
+    }, {
+        title: "¿Ya sabias que te amo mucho?",
+        desc: "En mis planes no esta lastimarte, quiero que vuelvas a creer en mi y confiar. quiero darte el trato que te mereces, nunca dudes de mi, desde que te conoci cambiaste mi vida por completo."
+    }, {
+       title: "",
+       desc: "Me considero el chico mas feliz estando a tu lado, y bueno, llevamos 91 dias juntos(3M), pero para mi llevamos mucho mas. Entre mas pasa el tiempo mas me gustas y mas me enamoro de ti,  "
+    }, {
+        title: "",
+        desc: "Y sere sincero, estoy tan enamorado de ti que me pierdo mucho, te pienso todos los dias, y me pongo triste si no apareces para hablarme."
+    }, {
+        title: "Supongo que ahora sabes que te amo tanto..",
+        desc: "¿Sabias que me gusta un poco la Astronomia? Me encanta ver la luna, las estrellas.. Siempre que miro me pongo a pensar si en realidad es algo automatico, no se si tendras alguna conexion con las estrellas, pero cada vez que las veo me recuerdan a ti, por lo lindas que son."
+ }];
+    var currentPage = 0;
+    //generate content
+    for (var i = 0; i < content.length; i++) {
+        //split content letters to array
+        for (var obj in content[i]) {
+            //if string
+            if (typeof content[i][obj] === "string") {
+                content[i][obj] = content[i][obj].split("");
+                continue;
+            }
+            //if array (grouped text)
+            else if (typeof content[i][obj] === "object") {
+                var toPush = [];
+                for (var j = 0; j < content[i][obj].length; j++) {
+                    for (var k = 0; k < content[i][obj][j].length; k++) {
+                        toPush.push(content[i][obj][j][k]);
+                    }
+                }
+                content[i][obj] = toPush;
+            }
+        }
+        //set text to 
+        $("#segments").append("<div class=\"letters-wrap mutable\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
+        setText();
+        //clone to data
+        $("#segments").append("<div class=\"letters-wrap position-data\"><div class=\"soup-title\"></div><div class=\"soup-desc\"></div></div>");
+        setText();
+    }
+    //initial arrangement
+    arrangeCurrentPage();
+    scrambleOthers();
+    /*
+     * Event handlers
+     */
+    $(window).resize(function () {
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    $("#soup-prev").hide();
+    $("#soup-prev").click(function () {
+        $("#soup-next").show();
+        currentPage--;
+        if (currentPage === 0) {
+            $("#soup-prev").hide();
+        }
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    $("#soup-next").click(function () {
+        $("#soup-prev").show();
+        currentPage++;
+        if (currentPage === content.length - 1) {
+            $("#soup-next").hide();
+        }
+        arrangeCurrentPage();
+        scrambleOthers();
+    });
+    /*
+     * Functions
+     */
+    function arrangeCurrentPage() {
+        for (var i = 0; i < content[currentPage].title.length; i++) {
+            $(".mutable:eq(" + currentPage + ") > .soup-title > .letter").eq(i).css({
+                left: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().left + "px",
+                top: $(".position-data:eq(" + currentPage + ") > .soup-title > .letter").eq(i).offset().top + "px",
+                color: "#111",
+                zIndex: 9001
+            });
+        }
+        for (var i = 0; i < content[currentPage].desc.length; i++) {
+            $(".mutable:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).css({
+                left: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().left + "px",
+                top: $(".position-data:eq(" + currentPage + ") > .soup-desc > .letter").eq(i).offset().top + "px",
+                color: "#111",
+                zIndex: 9001
+            });
+        }
+    }
+
+    function setText() {
+        var j;
+        for (j = 0; j < content[i].title.length; j++) {
+            $(".soup-title").last().append("<span class=\"letter\">" + content[i].title[j] + "</span>");
+        }
+        for (j = 0; j < content[i].desc.length; j++) {
+            $(".soup-desc").last().append("<span class=\"letter\">" + content[i].desc[j] + "</span>");
+        }
+    }
+
+    function scrambleOthers() {
+        for (var i = 0; i < content.length; i++) {
+            //don't scramble currentPage
+            if (currentPage === i)
+                continue;
+            var parts = [
+                ["title", ".soup-title"],
+                ["desc", ".soup-desc"]
+            ];
+            //apply to .title h1s and .desc ps
+            for (var j = 0; j < parts.length; j++) {
+                for (var k = 0; k < content[i][parts[j][0]].length; k++) {
+                    //define random position on screen
+                    var randLeft = Math.floor(Math.random() * $(window).width());
+                    var randTop = Math.floor(Math.random() * $(window).height());
+                    //defining boundaries
+                    var offset = $(".position-data").eq(currentPage).offset();
+                    var bounds = {
+                        left: offset.left,
+                        top: offset.top,
+                        right: $(window).width() - offset.left,
+                        bottom: $(window).height() - offset.top
+                    };
+                    var middleX = bounds.left + $(".position-data").eq(currentPage).width() / 2;
+                    var middleY = bounds.top + $(".position-data").eq(currentPage).height() / 2;
+                    //finally, apply all the scrambles
+                    $(".mutable:eq(" + i + ") > " + parts[j][1] + " > .letter").eq(k).css({
+                        left: randLeft,
+                        top: randTop,
+                        color: "#DDD",
+                        zIndex: "initial"
+                    });
+                }
+            }
+        }
+    }
+});
